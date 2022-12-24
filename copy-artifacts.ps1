ee8bb0ce2b80eac2a8efb4372e237e456b9290fe
Fred Emmott <git@fredemmott.com>
2022-12-24 16:14:14 -0600

    Generate metadata, including version

--- copy-artifacts.ps1
+++ copy-artifacts.ps1
@@ -12,6 +12,10 @@
 
     Directory to copy the artifacts to
 
+.PARAMETER BuildNumber
+
+    The 'd' in 'a.b.c.d'
+
 .EXAMPLE
 
   ./copy-artifacts -Configuration Debug 
@@ -26,16 +30,28 @@
 param(
   [ValidateSet('Debug', 'Release')]
   [string] $Configuration='Debug',
-  [string] $Out="$(Get-Location)",
-  [string] $Platform="x64"
+  [string] $Out="$(Get-Location)/out",
+  [string] $Platform="x64",
+  [int] $BuildNumber=0
 )
 
 If (!(Test-Path $Out)) {
   New-Item -ItemType Directory -Path $Out
 }
 
+$pluginVersion = "0.0.1.${BuildNumber}"
+
+$metadataJson = "${Out}/metadata.json"
+
+(Get-Content -Path OTDIPC/metadata.in.json) `
+  -Replace "@PLUGIN_VERSION@","${pluginVersion}" `
+  | Set-Content -Path $metadataJson
+
 $compress = @{
-  Path = "OTDIPC/bin/${Configuration}/net6.0/OpenKneeboard-OTDIPC.dll", "OTDIPC/metadata.json"
+  Path = @(
+    "OTDIPC/bin/${Configuration}/net6.0/OpenKneeboard-OTDIPC.dll",
+    $metadataJson
+  )
   DestinationPath = "${Out}\OpenKneeboard-OTD-IPC.zip"
   Force = $True
 }
