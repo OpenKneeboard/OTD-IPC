db39acf50ca5cbe3bdc272ca64d7ea1a2a68111e
Fred Emmott <git@fredemmott.com>
2022-12-24 20:09:47 -0600

    set version to 0.1.0

--- copy-artifacts.ps1
+++ copy-artifacts.ps1
@@ -39,7 +39,7 @@ If (!(Test-Path $Out)) {
   New-Item -ItemType Directory -Path $Out
 }
 
-$pluginVersion = "0.0.1.${BuildNumber}"
+$pluginVersion = "0.1.0.${BuildNumber}"
 Set-Content $Out/version.txt -Value $pluginVersion
 
 $metadataJson = "${Out}/metadata.json"
