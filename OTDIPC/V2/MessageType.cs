25b8be1da401ab76df6b3e456e1a4bb9e8c3f823
Fred Emmott <git@fredemmott.com>
2026-01-05 09:00:06 -0600

    Add a `Custom` message type to v2 protocol

--- OTDIPC/V2/MessageType.cs
+++ OTDIPC/V2/MessageType.cs
@@ -3,7 +3,6 @@
  *
  * SPDX-License-Identifier: MIT
  */
-using System.Runtime.InteropServices;
 
 namespace OTDIPC.V2
 {
@@ -12,6 +11,19 @@ namespace OTDIPC.V2
         DeviceInfo = 1,
         State = 2,
         Ping = 3,
+
+        // { Header, Size, char... }
         DebugMessage = 4,
+
+        /* { Header, Size, GUID, ... }
+         *
+         * The actual message type is identified by the GUID.
+         *
+         * GUID should be in the Win32/C# GUID struct binary layout, i.e. 16 bytes of endian-sensitive binary data
+         *
+         * Please consider contributing a new message type to OTD-IPC instead; this message type is primarily intended
+         * to support private experimentation without a risk of conflicts.
+         */
+        Experimental = 5,
     }
 }
--- include/OTD-IPC/MessageType.hpp
+++ include/OTD-IPC/MessageType.hpp
@@ -13,7 +13,22 @@ namespace OTDIPC::inline V2::Messages {
     DeviceInfo = 1,
     State = 2,
     Ping = 3,
+
+    // { header, size, char... }
     DebugMessage = 4,
+
+    /* { header, size, GUID, ... }
+     *
+     * The actual message type is identified by the GUID.
+     *
+     * GUID should be in the Win32/C# GUID struct binary layout, i.e. 16 bytes
+     * of endian-sensitive binary data
+     *
+     * Please consider contributing a new message type to OTD-IPC instead; this
+     * message type is primarily intended to support private experimentation
+     * without a risk of conflicts.
+     */
+    Experimental = 5,
   };
 
 }
